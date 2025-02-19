e5f588bd58c6a14974ac65269c3e41ae32e9d58c
RenatoSN12 <rsnascimento159@gmail.com>
2025-02-19 19:30:28 -0300

    :construction: Trabalhando no desenvolvimento em Blazor Server.

--- BlazorShop/obj/Debug/net9.0/BlazorShop.AssemblyInfo.cs
+++ BlazorShop/obj/Debug/net9.0/BlazorShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+918f271c7569209008359a1f3120b1d384efb4bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc189c0a3eb392a15e953a4ffe269b39666e2a57")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
