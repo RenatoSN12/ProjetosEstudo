5a0f051b2be2220eb75bb6a89a6a5ebc9b5033e1
RenatoSN12 <rsnascimento159@gmail.com>
2025-02-25 13:25:20 -0300

    Novos ajustes no Header

--- project-notion/src/components/layout/Header.js
+++ project-notion/src/components/layout/Header.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { IoIosMenu } from "react-icons/io";
 import logo from "../../img/logo.png";
-import Navbar from "./Navbar";
 import styles from "./Header.module.css";
 
 function Header() {
@@ -13,16 +12,10 @@ function Header() {
 
   return (
     <header className={styles.header}>
-      <div className={styles.menuIcon} onClick={toggleMenu}>
-        <IoIosMenu size={30} />
-      </div>
-
       <div className={`${styles.logo} ${isMenuOpen ? styles.logoHidden : ""}`}>
         <img src={logo} alt="logo-notion"/>
         <h1>Notion do Erick</h1>
       </div>
-
-      <Navbar isMenuOpen={isMenuOpen} />
     </header>
   );
 }
