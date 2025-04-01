0983e6740de9a34d98543663ca0f44fb6af3e20f
rsnascimento159@gmail.com <1597532486Re*>
2025-04-01 16:48:32 -0300

    Adicionados novos arquivos stripe

---
+++ Dima/Dima.Core/Requests/Stripe/GetTransactionsByOrderNumberRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dima.Core.Requests.Stripe
+{
+    public class GetTransactionsByOrderNumberRequest : Request
+    {
+        public string Number { get; set; } = string.Empty;
+        public string ProductTitle { get; set; } = string.Empty;
+        public string ProductDescription { get; set; } = string.Empty;
+        public long OrderTotal { get; set; }
+    }
+}
