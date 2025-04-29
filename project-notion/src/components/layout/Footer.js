dc97a40dfe11a3eba7473a8526c932354038d32b
RenatoSN12 <rsnascimento159@gmail.com>
2025-04-29 15:20:47 -0300

    Ajuste visibilidade do botão 'Comece Agora

--- project-notion/src/components/layout/Footer.js
+++ project-notion/src/components/layout/Footer.js
@@ -5,8 +5,8 @@ function Footer(){
     return (
         <footer className={styles.footer}>
         <ul className={styles.social_list}>
-            <li><a href='https://www.instagram.com/prosperefinance/' target='_blank' rel='noopener noreferrer'><FaLinkedin/></a></li>
-            <li><a href='https://www.linkedin.com/in/renato-nascimento-82b334210/' target='_blank' rel='noopener noreferrer'><FaInstagram/></a></li>
+            <li><a href='https://www.linkedin.com/in/renato-nascimento-82b334210/' target='_blank' rel='noopener noreferrer'><FaLinkedin/></a></li>
+            <li><a href='https://www.instagram.com/prosperefinance/' target='_blank' rel='noopener noreferrer'><FaInstagram/></a></li>
         </ul>
 
         <p className={styles.copy_right}>
