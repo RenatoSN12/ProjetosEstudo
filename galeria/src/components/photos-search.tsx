380a7836aaaf38d44b54f7e77d7dd4b67cf4e384
Renato Nascimento <desenvolvimento.transobra5@gmail.com>
2025-08-18 20:46:46 -0300

    Adicionado componente photo-widget.tsx

--- galeria/src/components/photos-search.tsx
+++ galeria/src/components/photos-search.tsx
@@ -1,14 +1,21 @@
 import InputText from "./input-text";
 import SearchIcon from "../assets/icons/search.svg?react"
 import React from "react";
+import { debounce } from "../helpers/utils";
 
 export default function PhotosSearch() {
 
     const [inputValue, setInputValue] = React.useState("");
 
-    function handleInputChange(e: React.ChangeEvent<HTMLInputElement>){
-        const value = e.target.value;
-    }
+    // const debouncedSetValue = React.useCallback(
+    //     debounce((value:string) )
+    // )
+
+    // function handleInputChange(e: React.ChangeEvent<HTMLInputElement>){
+    //     const value = e.target.value;
+
+        
+    // }
 
     return (
         <InputText
--- galeria/src/pages/page-home.tsx
+++ galeria/src/pages/page-home.tsx
@@ -1,5 +1,57 @@
-export default function PageHome(){
-    return (
-        <div></div>
-    )
+import Container from "../components/container";
+import PhotoWidget from "../contexts/photos/components/photo-widget";
+
+export default function PageHome() {
+  return (
+    <>
+      <Container>
+        <div className="grid grid-cols-4 gap-4">
+          <PhotoWidget
+            photo={{
+              id: "123",
+              title: "Olá mundo",
+              imageId: "portrait-tower.png",
+              albums: [
+                { id: "123", title: "Teste" },
+                { id: "321", title: "Teste2" },
+              ],
+            }}
+          />
+          <PhotoWidget
+            photo={{
+              id: "123",
+              title: "Olá mundo",
+              imageId: "portrait-tower.png",
+              albums: [
+                { id: "123", title: "Teste" },
+                { id: "321", title: "Teste2" },
+              ],
+            }}
+          />
+          <PhotoWidget
+            photo={{
+              id: "123",
+              title: "Olá mundo",
+              imageId: "portrait-tower.png",
+              albums: [
+                { id: "123", title: "Teste" },
+                { id: "321", title: "Teste2" },
+              ],
+            }}
+          />
+          <PhotoWidget
+            photo={{
+              id: "123",
+              title: "Olá mundo",
+              imageId: "portrait-tower.png",
+              albums: [
+                { id: "123", title: "Teste" },
+                { id: "321", title: "Teste2" },
+              ],
+            }}
+          />
+        </div>
+      </Container>
+    </>
+  );
 }
